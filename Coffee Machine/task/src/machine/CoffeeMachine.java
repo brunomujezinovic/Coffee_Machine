0e5b47dc0e599d1e9181856a11464d736804c54d
brunomujezinovic <brunomujezinovic@gmail.com>
2021-09-21 16:04:55 +0200

    CoffeMachine 3/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,42 @@
 package machine;
+
 import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need:");
+
+
+        // System.out.println("For 125 cups of coffee you will need:");
+        System.out.println("Write how many ml of water the coffee machine has:");
         Scanner s = new Scanner(System.in);
-        int a= s.nextInt();
-        System.out.println("For 125 cups of coffee you will need:");
-        System.out.println( a* 200 +" ml of water");
-        System.out.println( a* 50 +" ml of milk");
-        System.out.println( a* 15 +" g of coffee beans");
+        int water = s.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = s.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffee = s.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = s.nextInt();
+
+        int rwater = water / 200;
+        int rmilk = milk / 50;
+        int rcoffee = coffee / 15;
+
+        int smallest = Math.min(rwater, Math.min(rmilk, rcoffee));
+
+
+        if (cups < smallest) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + (smallest - cups) + " more than that)");
+        }
+        if (cups > smallest) {
+            System.out.println("No, I can make only " + smallest + " cup(s) of coffee");
+        }
+        if (smallest == cups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        }
+
+
     }
 }
