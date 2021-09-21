ea182dc4adc780bf57988d614d62d7920809cb9b
brunomujezinovic <brunomujezinovic@gmail.com>
2021-09-21 16:02:47 +0200

    CoffeMachine 2/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,13 @@
 package machine;
-
+import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        System.out.println("Write how many cups of coffee you will need:");
+        Scanner s = new Scanner(System.in);
+        int a= s.nextInt();
+        System.out.println("For 125 cups of coffee you will need:");
+        System.out.println( a* 200 +" ml of water");
+        System.out.println( a* 50 +" ml of milk");
+        System.out.println( a* 15 +" g of coffee beans");
     }
 }
